f388fc83585e0d9c7a11372443dfbd75ba4520b9
thenatural86 <philip.smalls86@gmail.com>
2021-02-24 12:24:16 -0500

    todo template literal

--- index.ts
+++ index.ts
@@ -2,4 +2,11 @@ import axios from 'axios'
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-axios.get(url).then((response) => console.log(response.data))
+axios.get(url).then((response) => {
+  const todo = response.data
+  const { id, title, finished } = todo
+
+  console.log(`the todo with id: ${id}
+  has a title of: ${title}.
+  Is it finished? ${finished}`)
+})
