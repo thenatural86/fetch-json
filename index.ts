91882107f4e1604c6ad2abf96189c81b2a7ba2a3
thenatural86 <philip.smalls86@gmail.com>
2021-02-24 12:39:19 -0500

    parameter types

--- index.ts
+++ index.ts
@@ -2,11 +2,22 @@ import axios from 'axios'
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1'
 
-axios.get(url).then((response) => {
-  const todo = response.data
-  const { id, title, finished } = todo
+interface Todo {
+  id: number
+  title: string
+  completed: boolean
+}
 
-  console.log(`the todo with id: ${id}
-  has a title of: ${title}.
-  Is it finished? ${finished}`)
+axios.get(url).then((response) => {
+  const todo = response.data as Todo
+  const { id, title, completed } = todo
+
+  logTodo(id, title, completed)
 })
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  the todo with id: ${id}
+  has a title of: ${title}.
+  Is it finished? ${completed}`)
+}
